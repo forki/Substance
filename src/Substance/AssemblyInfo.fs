aa4aeafd18fa6d9301aeb4e05c55bcd65f5e750f
devboy <dominic.graefen@gmail.com>
2015-08-23 02:17:28 +0200

    Init

---
+++ src/Substance/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("Substance")>]
+[<assembly: AssemblyProductAttribute("Substance")>]
+[<assembly: AssemblyDescriptionAttribute("...")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
