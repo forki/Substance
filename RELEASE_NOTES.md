29990efed47842d8f5db3185a4725bdb073e577c
devboy <dominic.graefen@gmail.com>
2015-08-23 10:22:40 +0200

    Bump version to 0.0.3-beta

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,3 +1,6 @@
+#### 0.0.3-beta - August 23 2015
+* Tuple
+
 #### 0.0.2-beta - August 23 2015
 * Tuple
 
--- src/Substance/Properties/AssemblyInfo.cs
+++ src/Substance/Properties/AssemblyInfo.cs
@@ -5,13 +5,13 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTitleAttribute("Substance")]
 [assembly: AssemblyProductAttribute("Substance")]
 [assembly: AssemblyDescriptionAttribute("...")]
-[assembly: AssemblyVersionAttribute("0.0.2")]
-[assembly: AssemblyFileVersionAttribute("0.0.2")]
+[assembly: AssemblyVersionAttribute("0.0.3")]
+[assembly: AssemblyFileVersionAttribute("0.0.3")]
 [assembly: InternalsVisibleToAttribute("Substance.Collections")]
 [assembly: InternalsVisibleToAttribute("Substance.Collections.Generic")]
 [assembly: InternalsVisibleToAttribute("Substance.Collections.Immutable")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "0.0.2";
+        internal const string Version = "0.0.3";
     }
 }
